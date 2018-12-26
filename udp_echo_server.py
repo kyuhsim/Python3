2ea87387a472eb0eb63dc4775f10caede08f3db1
Martin Sim <kyuhsim@naver.com>
2018-12-26 10:50:43 +0900

    khsim 20181226 - changed print to fit with python3.

--- udp_echo_server.py
+++ udp_echo_server.py
@@ -1,10 +1,11 @@
 '''
     UDP Echo server / client
     Server Program : running on controller_board
-	(using Python 2.7)
-	
+	(using Python 2.7 -> 3.5)
+
     History:
     20180720 kyuhsim - created.
+    20181226 kyuhsim - change printf to fit with python 3.
 '''
 
 import sys
@@ -26,21 +27,27 @@ if __name__ == '__main__':
 
     #bind the socket to the port
     server_address = ('localhost', gPort )
-    print >> sys.stderr, '- Starting server on %s port %s' % (server_address, gPort)
+#    print >> sys.stderr, '- Starting server on %s port %s' % (server_address, gPort)
+    print("- Starting server on ", server_address, " port ", gPort, end='\n', file=sys.stderr, flush=False)
     sock.bind( server_address )
 
     while True:
-      print >> sys.stderr, '\n- Waiting to receive message'
+#      print >> sys.stderr, '\n- Waiting to receive message'
+      print("\n- Waiting to receive message", end='\n', file=sys.stderr, flush=False)
       data, address = sock.recvfrom( 4096 )
 
-      print >> sys.stderr, '- received %d bytes from %s : ' % (len(data), address)
-      print >> sys.stderr, data
+#      print >> sys.stderr, '- received %d bytes from %s : ' % (len(data), address)
+      print("- received ", len(data), " bytes from ", address, end='\n', file=sys.stderr, flush=False)
+#      print >> sys.stderr, data
+      print(data, end='\n', file=sys.stderr, flush=False)
 
       if data:
         sent = sock.sendto( data, address )
-        print >> sys.stderr, '- sent %d bytes back to %s' % (sent, address)
+#        print >> sys.stderr, '- sent %d bytes back to %s' % (sent, address)
+        print("- sent ", sent, " bytes back to ", address, end='\n', file=sys.stderr, flush=False)
 
 
-  print >> sys.stderr, '- Program %s exited normally' % sys.argv[0]
+#  print >> sys.stderr, '- Program %s exited normally' % sys.argv[0]
+  print("- Program ", sys.argv[0], " exited normally", end='\n', file=sys.stderr, flush=True)
 
 # End of File
